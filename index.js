41ed0d996e2717407de363c2b0e6cfca5c022191
AlexARNcode <alexarnout@gmail.com>
2022-07-04 11:47:05 +0200

    Connect to the mysql database using a config file

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-
 import booksRoutes from './routes/books.js'
+import mysql from 'mysql';
+import dbConfig from './config/config.js';
 
 const app = express();
 const PORT = 8080;
+
+const db = mysql.createConnection(dbConfig);
+
+  db.connect(function(err) {
+    if (err) throw err;
+    console.log("Connecté à la base de données MySQL!");
+  });
 
 app.use(bodyParser.json())
 app.use('/books', booksRoutes);
