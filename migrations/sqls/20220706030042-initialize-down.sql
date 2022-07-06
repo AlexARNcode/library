8fbc112f0492a2b131649af906a7d07c1c14c037
AlexARNcode <alexarnout@gmail.com>
2022-07-06 06:56:29 +0200

    Remove useless files

--- migrations/sqls/20220706030042-initialize-down.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE books
-(
-	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT COMMENT "id of the book",
-    name VARCHAR(255) NOT NULL COMMENT "name of the book",
-    year INT NOT NULL COMMENT "publication's year of the book",
-    category VARCHAR(255) COMMENT "category of the book"
-);
