bd48beafb638b2cf58e939e5db2ddc24c0cb4512
AlexARNcode <alexarnout@gmail.com>
2022-07-06 04:27:51 +0200

    Allow a PUT call only if all parameters are defined. If not, throw an error message

--- controllers/books.js
+++ controllers/books.js
@@ -38,13 +38,19 @@ export const getBook = (req, res) => {
 export const updateBook = (req, res) => {
     const { name, year, category } = req.body
 
+    if (name === undefined || year === undefined || category === undefined) {
+      const errorMessage = "Parameters 'name', 'year' and 'category' must be defined to update a book !";
+      res.status(500).send(errorMessage);
+      throw new Error(errorMessage);
+    } 
+
     const sql = "UPDATE books SET name = ?, year = ?, category = ? WHERE id = ?";
     const sqlParams = [name, year, category, req.params.id];
 
     db.query(sql, sqlParams, function (err, result) {
       if (err) throw err;
       res.send({message: `The book '${name}' has been updated.`})
-    });
+    }); 
 }
 
 export const deleteBook = (req, res) => {
