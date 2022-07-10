acafeee39440f2d838a22e2a4cfbe0e05d872e74
AlexARNcode <alexarnout@gmail.com>
2022-07-10 08:33:15 +0200

    Update NewBooks.js: Improve input form for new book (UI)

--- front/src/Components/NewBook.js
+++ front/src/Components/NewBook.js
@@ -1,6 +1,6 @@
 import { Container } from "@mui/system";
 import { Link } from "react-router-dom";
-import { TextField, Box } from "@mui/material";
+import { TextField, Box, Typography } from "@mui/material";
 import React from "react";
 import CustomButton from "./CustomButton";
 import { useState } from "react";
@@ -21,35 +21,49 @@ export default function NewBook() {
                             Retour
                 </Link>
             </Box>
-          
+
+            <Typography align="center" variant="h2" sx={{ mb: 3 }}>
+                New book
+            </Typography>
+  
+                <Box>
                 <TextField 
-                    value={bookName}
-                    onChange={(e) => {
-                        setBookName(e.target.value);
-                      }}
-                    id="outlined-basic" 
-                    label="Name" 
-                    variant="outlined" />
+                        value={bookName}
+                        onChange={(e) => {
+                            setBookName(e.target.value);
+                        }}
+                        id="outlined-basic" 
+                        label="Name" 
+                        variant="outlined" />
+                </Box>
+                <Box>
+                    <TextField 
+                            value={bookYear}
+                            onChange={(e) => {
+                                setBookYear(e.target.value);
+                            }}
+                            id="outlined-basic" 
+                            label="Publication year" 
+                            variant="outlined" />
+                </Box>
+                <Box>
                 <TextField 
-                    value={bookYear}
-                    onChange={(e) => {
-                        setBookYear(e.target.value);
-                      }}
-                    id="outlined-basic" 
-                    label="Publication year" 
-                    variant="outlined" />
-                <TextField 
-                    value={bookCategory}
-                    onChange={(e) => {
-                        setBookCategory(e.target.value);
-                      }}
-                    id="outlined-basic" 
-                    label="Category" 
-                    variant="outlined" />
-                <CustomButton 
-                    text="SAVE" 
-                    onClick={() => addBook(bookName, bookYear, bookCategory)}>
-                </CustomButton>
+                        value={bookCategory}
+                        onChange={(e) => {
+                            setBookCategory(e.target.value);
+                        }}
+                        id="outlined-basic" 
+                        label="Category" 
+                        variant="outlined" />
+                </Box>
+                    
+                    <CustomButton 
+                        text="SAVE" 
+                        sx={{ mt: 3 }}
+                        onClick={() => addBook(bookName, bookYear, bookCategory)}>
+                    </CustomButton>
+           
+                
         </Container>
     );      
 }
