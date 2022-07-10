9db9bbe650e97cf6b6c7cf503e8038fee46190b8
AlexARNcode <alexarnout@gmail.com>
2022-07-10 07:42:05 +0200

    Create api helpers

---
+++ front/src/helper/api/addBook.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+export default function addBook(name, year, category) {
+    axios.post('http://localhost:8080/books', {
+        name: name,
+        year: year,
+        category: category
+      })
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+}
