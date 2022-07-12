435f55b065200fa6332892cbd10aecf2c3d9ef07
AlexARNcode <alexarnout@gmail.com>
2022-07-12 09:10:33 +0200

    Add 'create a new user' feature

--- controllers/users.js
+++ controllers/users.js
@@ -1,8 +1,19 @@
 import mysql from 'mysql';
 import dbConfig from '../config/dbConfig.js';
+import { findUserByEmail, insertNewUser } from '../repositories/usersRepository.js';
 
 const db = mysql.createConnection(dbConfig);
 
 export const createUser = (req, res) => {
-    res.send("Create user");
+    const userEmail = req.body.email
+
+    findUserByEmail(userEmail, function(result) {
+        if (result.length !== 0) {
+            res.send("User already exists !")
+        } else {
+            insertNewUser(userEmail, req.body.password, function(result) {
+                res.send(result)
+            })    
+        }    
+    })
 }
---
+++ repositories/usersRepository.js
@@ -0,0 +1,26 @@
+import mysql from 'mysql';
+import dbConfig from '../config/dbConfig.js';
+
+const db = mysql.createConnection(dbConfig);
+
+export function findUserByEmail(email, callback) {
+    const sql = "SELECT * FROM users WHERE email = ?";
+    const sqlParams = [email];
+
+    return db.query(sql, sqlParams, function (err, result) {
+      if (err) throw err;
+      return callback(result)
+    })
+}
+
+export function insertNewUser(email, password, callback) {
+    const sql = "INSERT INTO users (email, password) VALUES (?, ?)";
+    const sqlParams = [email, password];
+
+    db.query(sql, sqlParams, function (err, result) {
+        if (err) throw err;
+        return callback(result)
+      });
+}
+
+export default {findUserByEmail, insertNewUser}
